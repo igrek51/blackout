f33201abf943fb85187548d114d6f1f428f05b69
igrek51 <igrek51.dev@gmail.com>
2022-03-12 00:05:29 +0100

    Add android tv launcher

--- app/src/main/java/dev/igrek/blackout/activity/AppInitializer.kt
+++ app/src/main/java/dev/igrek/blackout/activity/AppInitializer.kt
@@ -2,6 +2,11 @@ package dev.igrek.blackout.activity
 
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Handler
+import android.provider.Settings
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
@@ -19,9 +24,9 @@ import kotlinx.coroutines.withContext
 
 
 class AppInitializer(
-    windowManagerService: LazyInject<WindowManagerService> = appFactory.windowManagerService,
-    activityController: LazyInject<ActivityController> = appFactory.activityController,
-    activity: LazyInject<Activity> = appFactory.activity,
+        windowManagerService: LazyInject<WindowManagerService> = appFactory.windowManagerService,
+        activityController: LazyInject<ActivityController> = appFactory.activityController,
+        activity: LazyInject<Activity> = appFactory.activity,
 ) {
     private val windowManagerService by LazyExtractor(windowManagerService)
     private val activityController by LazyExtractor(activityController)
@@ -70,6 +75,55 @@ class AppInitializer(
         val windowAttr: WindowManager.LayoutParams = activity.window.attributes
         windowAttr.screenBrightness = 0f
         activity.window.attributes = windowAttr
+
+        try {
+            checkSystemWritePermission()
+        } catch (e: Throwable) {
+            Toast.makeText(activity, "Error: $e", Toast.LENGTH_LONG).show()
+        }
+
+        Handler().postDelayed(this::dimScreen, 3000)
+    }
+
+    private fun dimScreen() {
+        try{
+            val cResolver = activity.contentResolver
+            val window = activity.window
+
+            // To handle the auto
+            Settings.System.putInt(
+                    cResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+            )
+            // Get the current system brightness
+            val brightness = Settings.System.getInt(
+                    cResolver, Settings.System.SCREEN_BRIGHTNESS
+            )
+
+            // Set the system brightness using the brightness variable value
+            Settings.System.putInt(
+                    cResolver, Settings.System.SCREEN_BRIGHTNESS, 0
+            )
+        } catch (e: Throwable) {
+            Toast.makeText(activity, "Error: $e", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkSystemWritePermission(): Boolean {
+        var retVal = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            retVal = Settings.System.canWrite(activity)
+            if (retVal) {
+                Toast.makeText(activity, "System Settings Write allowed", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(activity, "System Settings Write not allowed", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+                intent.setData(Uri.parse("package:dev.igrek.blackout"));
+                activity.startActivity(intent)
+            }
+        }
+        return retVal
     }
 
     private fun handleError(t: Throwable) {
